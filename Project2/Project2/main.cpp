01922c01b373b63a014e988df41f9dbefc5a5b39
PowerOleg <geraskin.oleg@mail.ru>
2024-08-17 10:24:19 +0300

    task2;Two threads

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -5,20 +5,20 @@
 #include <algorithm>
 #include <numeric>
 
-std::vector<int> summ_vectors(const std::vector<int>& v1, const std::vector<int>& v2, long size)
+void summ_vectors(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int> &result, long from, long size)
 {
-	std::vector<int> result(size);
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = from; i < size; i++)
 	{
 		result[i] = v1[i] + v2[i];
 	}
-	return result;
 }
 
-double thread1(const std::vector<int>& v1000)
+double thread_1(const std::vector<int>& v1000, long size)
 {
+	std::vector<int> result(size);
+
 	auto start = std::chrono::steady_clock::now();
-	std::thread t1(summ_vectors, v1000, v1000, 1000L);
+	std::thread t1(summ_vectors, v1000, v1000, std::ref(result), 0, size);
 	if (t1.joinable())
 	{
 		t1.join();
@@ -31,6 +31,29 @@ double thread1(const std::vector<int>& v1000)
 	return d_time;
 }
 
+double threads_2(const std::vector<int>& v1000, long size)
+{
+	std::vector<int> result(size);
+
+	auto start = std::chrono::steady_clock::now();
+	long half = size / 2;
+	std::thread t1(summ_vectors, v1000, v1000, std::ref(result), 0, half);
+	std::thread t2(summ_vectors, v1000, v1000, std::ref(result), half, size);
+	if (t1.joinable())
+	{
+		t1.join();
+	}
+	if (t2.joinable())
+	{
+		t2.join();
+	}
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<long, std::nano> time = end - start;
+
+	double d_time = static_cast<double>(time.count()) / 1000000000;
+	return d_time;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -39,25 +62,40 @@ int main(int argc, char** argv)
 	std::cout << "Quantity of processor cores - " << cores_quantity << std::endl << std::endl;
 	std::cout << "\t" << "1000" << "\t" << "\t" << "10000" << "\t" << "\t" << "100000" << "\t" << "\t" << "1000000" << "\t" << std::endl;
 
-
-	std::vector<int> v1000(1000);
+	long size1000 = 1000L;
+	long size10000 = 10000L;
+	long size100000 = 100000L;
+	long size1000000 = 1000000L;
+	std::vector<int> v1000(size1000);
 	std::iota(std::begin(v1000), std::end(v1000), 0);
-	std::vector<int> v10000(10000);
+	std::vector<int> v10000(size10000);
 	std::iota(std::begin(v10000), std::end(v10000), 0);
-	std::vector<int> v100000(100000);
+	std::vector<int> v100000(size100000);
 	std::iota(std::begin(v100000), std::end(v100000), 0);
-	std::vector<int> v1000000(1000000);
+	std::vector<int> v1000000(size1000000);
 	std::iota(std::begin(v1000000), std::end(v1000000), 0);
 
 	//quantity of threads = 1
-	double result_time1 = thread1(v1000);
-	std::cout << "1 thread "  << result_time1 << "s" << "\t" << 0.9999 << std::endl;
-
+	double result_time1000 = thread_1(v1000, size1000);
+	double result_time10000 = thread_1(v10000, size10000);
+	double result_time100000 = thread_1(v100000, size100000);
+	double result_time1000000 = thread_1(v1000000, size1000000);
+	std::cout << "1 thread "  << result_time1000 << "s" << "\t" << result_time10000 << "s" << "\t" << result_time100000 << "s" << "\t" << result_time1000000 << "s" << std::endl;
 
 	//quantity of threads = 2
-
+	double result_time1000_2 = threads_2(v1000, size1000);
+	double result_time10000_2 = threads_2(v10000, size10000);
+	double result_time100000_2 = threads_2(v100000, size100000);
+	double result_time1000000_2 = threads_2(v1000000, size1000000);
+	std::cout << "2 threads " << result_time1000_2 << "s" << "\t" << result_time10000_2 << "s" << "\t" << result_time100000_2 << "s" << "\t" << result_time1000000_2 << "s" << std::endl;
+	
 	//quantity of threads = 4
-
+	/*double result_time1000_4 = threads_4(v1000, size1000);
+	double result_time10000_4 = threads_4(v10000, size10000);
+	double result_time100000_4 = threads_4(v100000, size100000);
+	double result_time1000000_4 = threads_4(v1000000, size1000000);
+	std::cout << "4 threads " << result_time1000_4 << "s" << "\t" << result_time10000_4 << "s" << "\t" << result_time100000_4 << "s" << "\t" << result_time1000000_4 << "s" << std::endl;
+	*/
 	//quantity of threads = 8
 
 	//quantity of threads = 16
